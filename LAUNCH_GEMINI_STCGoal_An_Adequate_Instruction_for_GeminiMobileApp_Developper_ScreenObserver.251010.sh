a72e30f1d1a1904ee1543521cd568933c3e94498
JGWill <jgi@jgwill.com>
2025-10-10 09:14:56 -0400

    files of issue #58

---
+++ LAUNCH_GEMINI_STCGoal_An_Adequate_Instruction_for_GeminiMobileApp_Developper_ScreenObserver.251010.sh
@@ -0,0 +1,17 @@
+_GEMINII_DEFAULT_MODEL_set pro
+
+geminiid "
+
+I will need a special new agent that the instructions would be given to the iOS mobile app with which we would share the screen and do activities such as : Observe a Github feature description , Observe a web user interface of one of our prototype to create a full prompt to generate the next version, observe a web user interface (or PWA (kind of Web App we install on our Home screen) to produce a user experience report that would lead to an adjustment prompt and anything else you have in mind (might need to see in /src/llms what we have, maybe the MMOT and other guidance might help along with other agents in here such as @api-documenter.md @architect-review.md @business-analyst.md @ui-ux-designer.md @prompt-engineer.md @mobile-developer.md @ios-developer.md @devops-troubleshooter.md @frontend-developer.md @reality-narrative-researcher.md etc.  Please notes, these are not necessarely precisely those to use but to give you idea of what I identified immediatly. An example that I will review is the Web App in /src/IAIP/app if that help you by giving you a case.  I temporarely brought your embodiment with Ava in @GEMINI.Ava.md so see how you could bring what is relevant of her in your @GEMINI.md - I guess it is important that in this new file (or maybe many) we are capable to give that to gemini iOS app as initial prompt so he would understand his role then we share the screen and start observing with him what we do have and produce a report at the end that is like the content of the github issue for the next steps.
+
+
+
+in @.hch/issues.json is a list of issues that were originally drafted for this repo, in @FEATURES.md was what was produced by another instance to know what we created in here.
+
+Few pointers :
+
+* We are not creating iOS app, we are just doing the observations on iOS using the Gemini iOS app.
+* We are not writing any code or implementing anything.  We are either observing inspiring site for features and ways to work to produce that in our own software, or we are observing our own prototype to produce a report that would lead to a prompt to adjust the prototype.
+* We end up Ava fully working in here and you migrated what is needed of her in your local GEMINI.md (no external dependency)
+
+" /src/llms/ /src/IAIP /src/_jgt/gaia/linux_migration/ /cesaret/book/_/tcc
